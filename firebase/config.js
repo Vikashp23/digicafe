7041ae7e02bb1fe2ca2db2930667a156ed194ff1
Vikashp23 <email.vikashpathak@gmail.com>
2026-08-15 18:43:27 +0530

    Initialize Firebase app and export services

--- firebase/config.js
+++ firebase/config.js
@@ -1,1 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-storage.js";
 
+const firebaseConfig = {
+  // your existing keys
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const storage = getStorage(app);
